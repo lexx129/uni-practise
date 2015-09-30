1af4183b3eb121874725a5e6c8b49eff6c207905
lexx129 <aleks04.94@gmail.com>
2015-09-30 08:45:31 +0300

    from 29.09

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,7 @@ import java.io.*;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
+import java.util.*;
 
 public class Main {
 
@@ -23,25 +22,41 @@ public class Main {
         return new BigInteger(instance.digest()).toString(16);
     }
 
-    private static FileInfo addInstance(File file) throws NoSuchAlgorithmException, IOException {
-//        File curr_file = new File(path);
-        if (file.isDirectory()){
-            String curr_hash =
-        }
-        else
-        String curr_hash = getHash(file);
-        return new FileInfo(file.length(), curr_hash);
+    private static String getDirHash(File file) throws NoSuchAlgorithmException {
+        MessageDigest instance = MessageDigest.getInstance("SHA-1");
+        byte[] b = file.getAbsolutePath().getBytes();
+        instance.reset();
+        instance.update(b);
+        return new BigInteger(instance.digest()).toString(16);
+    }
+
+    private static boolean checkHash(File checkTarget, String true_hash) throws NoSuchAlgorithmException {
+        String curr_hash = getHash(checkTarget);
+        return true_hash.equals(curr_hash);
 
     }
 
-    private static void scan(File rootDir) throws IOException, NoSuchAlgorithmException {
-        HashMap<String, FileInfo> map = new LinkedHashMap<>();
+    private static FileInfo addInstance(File file) throws NoSuchAlgorithmException, IOException {
+//        File curr_file = new File(path);
+        String curr_hash;
+        if (file.isDirectory()) {
+            curr_hash = getDirHash(file);
+        } else {
+            curr_hash = getHash(file);
+        }
+        return new FileInfo(file.length(), curr_hash);
+    }
+
+    private static void snapMake(File rootDir) throws IOException, NoSuchAlgorithmException {
+        TreeMap<String, FileInfo> map = new TreeMap<>();
         FileInfo fi;
         FileOutputStream fos = new FileOutputStream("snapshot.out");
         ObjectOutputStream oos = new ObjectOutputStream(fos);
 //        String rootDir = "d:\\";
 //        File root = new File(rootDir);
         File[] files = rootDir.listFiles();
+//        files[0] = rootDir;
+
         int i = 0;
         assert files != null;
         while (i < files.length) {
@@ -64,34 +79,129 @@ public class Main {
             files = temp;
             i++;
         }
+        long scanned = 0;
         for (File file : files) {
             System.out.println(file.getPath());
-            if (!file.isDirectory()) {
-                fi = addInstance(file);
-                map.put(file.getAbsolutePath(), fi);
-            }
+            scanned++;
+//            System.out.println("Files scanned: " + scanned);
+            fi = addInstance(file);
+            map.put(file.getAbsolutePath(), fi);
         }
+//        int a = 0 + 16;
+        System.out.println("Total: " + scanned);
         oos.writeObject(map);
         oos.flush();
         oos.close();
     }
 
-    private static void check (String rootDir) throws IOException, ClassNotFoundException {
-        FileInputStream fis = new FileInputStream("snapshot.out");
-        ObjectInputStream ois = new ObjectInputStream(fis);
-        LinkedHashMap<String, FileInfo> ref = (LinkedHashMap<String, FileInfo>) ois.readObject();
-        String root = ref.entrySet().iterator().next().getKey();
-        System.out.println(root);
-
+    private static boolean isInner(String root, String target) {
+        return target.contains(root);
     }
 
 
-    public static void main(String[] args) throws IOException, NoSuchAlgorithmException, ClassNotFoundException {
-        // enter a place for scan here
-        File file = new File("C:\\PortableTex");
-        scan(file);
-        check("C:\\PortableTex");
+    private static int findAndCheck(String curr_path, String curr_hash, Long curr_size) throws NoSuchAlgorithmException {
+        File temp = new File(curr_path);
+        boolean name = temp.getAbsolutePath().toLowerCase().equals(curr_path);
+        boolean hash = checkHash(temp, curr_hash);
+        boolean size = (curr_size.equals(temp.length()));
+        if (temp.exists()) {
+            if (name && hash && size) {
+                System.out.println("File + " + temp.getAbsolutePath() + " is absolutly OK");
+                return 1;
+            }
+            if (!name && hash && size) {
+                System.out.println("File + " + temp.getAbsolutePath() + " was renamed");
+                return 2;
+            }
+            if (name && !hash && size) {
+                System.out.println("File + " + temp.getAbsolutePath() + " was changed (different hash)");
+                return 3;
+            }
+            if (name && hash && !size) {
 
+
+            }
+        }
+
+        return 0;
+    }
+
+    private static void snapCheck(String checkPath) throws IOException, ClassNotFoundException {
+        FileInputStream fis = new FileInputStream("snapshot.out");
+        ObjectInputStream ois = new ObjectInputStream(fis);
+        TreeMap<String, FileInfo> ref = (TreeMap<String, FileInfo>) ois.readObject();
+        String preRoot = ref.entrySet().iterator().next().getKey();
+        String rootFolder = new File(preRoot).getParent().toLowerCase();
+//        System.out.println(checkPath + "   " + rootFolder);
+//        boolean isInner = checkPath.contains(rootFolder);
+//        boolean isOuter = rootFolder.contains(checkPath);
+        System.out.println("Ref size should be: " + ref.size());
+        if (isInner(rootFolder, checkPath)) {
+            System.out.println("**Path for checking is inner!");
+            System.out.println(ref.lastKey());
+            SortedMap<String, FileInfo> sub = ref.subMap(checkPath, ref.lastKey());
+//            Iterator iterator = sub.entrySet().iterator();
+            String lower_grade = "";
+            for (Map.Entry<String, FileInfo> entry : sub.entrySet()) {
+                String prev = lower_grade;
+                lower_grade = entry.getKey();
+//                String curr_hash = entry.getValue().getHash();
+                if (!lower_grade.contains(checkPath)) {
+                    lower_grade = prev;
+                    break;
+                }
+            }
+            System.out.println(lower_grade);
+            sub = ref.subMap(checkPath, lower_grade);
+//            System.out.println(ref.lastKey());
+//            for (int i = ref.keySet().; i < ; i++) {
+
+        }
+//        System.out.println(b);
+//        System.out.println("-----\r\n" + ololo);
+//        boolean b = ref.containsKey("C:\\SwSetup\\sp45820");
+//        System.out.println(b);
+
+    }
+
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException, ClassNotFoundException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Режим работы: \n 1 - Создание снимка указанного расположения " +
+                "\n 2 - Проверка по уже существующему снимку");
+        String type = bufferedReader.readLine();
+        switch (type) {
+            case "1":
+                System.out.println("Enter a place for scanning: ");
+                String rootPlace = bufferedReader.readLine();
+                File rootFile = new File(rootPlace);
+                snapMake(rootFile);
+//            snapCheck(rootFile);
+                break;
+            case "2":
+                System.out.println("Enter a place to check: ");
+                String checkPlace = bufferedReader.readLine();
+                snapCheck(checkPlace);
+                break;
+            case "3":
+                File temp1 = new File("C:\\SwSetup\\SoftPaq\\SP54421\\autorun45.inf");
+                System.out.println(getHash(temp1));
+                break;
+//            6e072801d977bfe94f38c2cad4cf3b3e419e8cb2
+            default:
+//                System.out.println(type);
+                System.err.println("Invalid working mode number");
+                System.exit(0);
+        }
+        // enter a place for snapMake here
+//        File file = new File("C:\\SwSetup");
+
+
+//        System.out.println(getDirHash(temp));
+//        System.out.println(temp.length());
+//        System.out.println(getHash(temp1));
+//        System.out.println(temp1.length());
+//        snapCheck("C:\\SwSetup\\SP58970");
+//        6e072801d977bfe94f38c2cad4cf3b3e419e8cb2
 
     }
 
